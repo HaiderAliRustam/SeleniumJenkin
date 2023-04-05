d16d9ebca84dd50eaa9e2fb1643e0d11c938ba22
haider Ali <hafizhaideraliuet@gmail.com>
2023-04-05 10:13:30 +0500

    ali

---
+++ ForJenkin/FileName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForJenkin
+{
+    public class FileName
+    {
+        [Fact]
+        public void test2()
+        {
+
+        }
+    }
+}
--- ForJenkin/UnitTest1.cs
+++ ForJenkin/UnitTest1.cs
@@ -1,14 +1,39 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
 
 namespace ForJenkin
 {
     public class UnitTest1
     {
 
-       public UnitTest1()       {
+        public UnitTest1()
+        {
+
+        }
+        [Fact]
+        public void Test1()
+        {
+
+            // Set chrome options to run in headless mode
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+
+
+
+            // Set the path of the new ChromeDriver executable
+            string chromeDriverPath = "C:\\Users\\hp\\Downloads\\Programs\\chromedriver_win32\\chromedriver.exe";
+
+
+
+            // Create an instance of the ChromeDriver class with the new path
+           // ChromeDriverService service = ChromeDriverService.CreateDefaultService(chromeDriverPath);
+            IWebDriver driver = new ChromeDriver(chromeOptions);
+
             // Create a new instance of the Chrome driver
-            IWebDriver driver = new ChromeDriver();
+            // IWebDriver driver = new ChromeDriver();
+
+
 
             // Navigate to the website
             driver.Navigate().GoToUrl("https://courses.rahulshettyacademy.com/");
@@ -17,14 +42,14 @@ namespace ForJenkin
             Thread.Sleep(5000);
 
             // Find the first button and click on it
-            IWebElement button1 = driver.FindElement(By.XPath("//a[normalize-space()='Courses']"));
-            button1.Click();
+            //IWebElement button1 = driver.FindElement(By.XPath("//a[normalize-space()='Courses']"));
+            //button1.Click();
 
             // Wait for the page to load
             Thread.Sleep(5000);
 
             // Find the second button and click on it
-           IWebElement button2 = driver.FindElement(By.XPath("//div[@title='All-Access Membership']"));
+            IWebElement button2 = driver.FindElement(By.XPath("//div[@title='All-Access Membership']"));
             button2.Click();
 
             // Wait for the page to load
@@ -32,11 +57,6 @@ namespace ForJenkin
 
             // Close the browser
             driver.Quit();
-        }
-        [Fact]
-        public void Test1()
-        {
-
 
         }
     }
